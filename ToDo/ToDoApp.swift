9952a5985af25a6250e8f61561869c9cb2148cb5
aysunm <aysun.molla@huawei.com>
2022-03-22 21:20:20 +0300

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by admin on 22.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
